fa20d919caeeb87dd8900367b7614a36d685f64e
John Patrick Madrigal <jp.madrigal07@gmail.com>
2023-06-19 18:38:27 +0800

    fix: Change to ms.s25.bet web

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,14 @@
+"use client"
 import Image from 'next/image'
+import { useEffect } from 'react';
 
 export default function Home() {
+  useEffect(() => {
+    window.location.href = "https://m.s26.bet";
+  }, []);
   return (
     <main className="flex justify-center">
-      <Image
-        src="/1024x5008.2.png"
-        alt="Next.js Logo"
-        width={1024}
-        height={5008}
-        priority
-      />
+      <h3 className="p-4">Loading...</h3>
     </main>
   )
 }
